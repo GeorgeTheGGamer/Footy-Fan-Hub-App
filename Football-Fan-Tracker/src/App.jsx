4af5c4b8fbb2ea9c24d2f4a08bd407cbd5e516f2
George Blagden <147810913+GeorgeTheGGamer@users.noreply.github.com>
2025-07-10 17:21:40 +0100

    Tailwind css and basic homepage set up

---
+++ Football-Fan-Tracker/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <header className='welcome-banner'>
+        <img src="./welcome-banner.jpg" alt="Welcome back Drop" />
+        <h1>Welcome to Footy Fan Hub</h1>
+        <p>Where you can follow your favourite team on the go</p>
+      </header>
+      <section>
+        <button>Select your team</button>
+      </section>
+    </main>
+  )
+}
+
+export default App
