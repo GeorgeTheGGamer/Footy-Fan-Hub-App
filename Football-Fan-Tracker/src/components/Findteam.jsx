9f3ae4e69af8b9c6addc3594faad8ff3569a8fda
George Blagden <147810913+GeorgeTheGGamer@users.noreply.github.com>
2025-07-12 21:40:59 +0100

    Team cards and Team Viewer

--- Football-Fan-Tracker/src/components/Findteam.jsx
+++ Football-Fan-Tracker/src/components/Findteam.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState,useEffect } from 'react'
 import Search from './Search'
+import Teamcard from './Teamcard'
 
 const Findteam = ({onClose }) => {
   const [isExit, setisExit] = useState(false)
@@ -17,9 +18,15 @@ const Findteam = ({onClose }) => {
     <main className='team-finder'>
         <header>
             <Search/>
-            <button onClick={() => setisExit(true)}>X</button>
-        
+            <button onClick={() => setisExit(true)}>X</button>  
         </header>
+        <section className='teamcard-view'>
+            
+            <div>
+                <Teamcard/>
+            </div>
+
+        </section>
         
         {/* Your team selection content here */}
     </main>
---
+++ Football-Fan-Tracker/src/components/Teamcard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Teamcard = () => {
+  return (
+    <div className='team-card'>
+        <div>
+            <img src="./spurs-logo.jpg" alt="Spurs Image" />
+            <p>SPURS</p>
+        </div>
+    </div>
+  )
+}
+
+export default Teamcard
