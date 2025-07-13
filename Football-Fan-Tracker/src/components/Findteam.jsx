4a3f8c713e741c02b74e02207897f93edbc77d71
George Blagden <147810913+GeorgeTheGGamer@users.noreply.github.com>
2025-07-13 19:38:57 +0100

    SportsDB API Usage

--- Football-Fan-Tracker/src/components/Findteam.jsx
+++ Football-Fan-Tracker/src/components/Findteam.jsx
@@ -3,6 +3,9 @@ import { useState,useEffect } from 'react'
 import Search from './Search'
 import Teamcard from './Teamcard'
 
+const API_BASE_URL = `https://www.thesportsdb.com/api/v1/json/123/`
+const API_KEY = import.meta.env.VITE_SPORTSDB_API_KEY
+
 const Findteam = ({onClose }) => {
   const [isExit, setisExit] = useState(false)
 
